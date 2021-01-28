c75b9f01615469d90a2b56740cc3c10a647cad2e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-28 13:41:57 +0100

    refactor: extract functionality into method

--- src/Quick Sell/ModEntry.cs
+++ src/Quick Sell/ModEntry.cs
@@ -24,21 +24,34 @@ namespace Quick_Sell
         {
             this.Config = this.Helper.ReadConfig<ModConfig>();
 
-            helper.Events.Input.ButtonPressed += this.OnSellButtonPressed;
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
         }
 
-        /// <summary>Raised after the player presses the quick sell button.</summary>
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void OnSellButtonPressed(object sender, ButtonPressedEventArgs e)
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
             if (!Context.IsWorldReady)
                 return;
 
-            if (e.Button != this.Config.SellKey)
+            //if (!Context.IsPlayerFree)
+            //    return;
+
+            //if (!Game1.displayHUD)
+            //    return;
+
+            if (e.Button == this.Config.SellKey)
+                this.OnSellButtonPressed(sender, e);
+        }
+
+        private void OnSellButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            Item item = GetHoveredItem();
                 return;
 
             this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        }
         }
     }
 }
